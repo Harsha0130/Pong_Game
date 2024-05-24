3030e047a09897c5d37e45070313f318ad47eb60
Harsha <harshahm0130@gmail.com>
2024-05-24 19:01:53 +0530

    Step-3

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from turtle import Screen, Turtle
-import time
+from turtle import Screen
+from paddle import Paddle
 
 scr = Screen()
 scr.tracer(0)
@@ -7,28 +7,14 @@ scr.bgcolor("black")
 scr.setup(width=800, height=600)
 scr.title("Pong Game")
 
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(5, 1)
-paddle.penup()
-paddle.goto(350, 0)
-
-
-def go_up():
-    new_y = paddle.ycor() + 30
-    paddle.goto(paddle.xcor(), new_y)
-
-
-def go_down():
-    new_y = paddle.ycor() - 30
-    paddle.goto(paddle.xcor(), new_y)
-
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 scr.listen()
-scr.onkey(go_up, "Up")
-scr.onkey(go_down, "Down")
-
+scr.onkey(r_paddle.go_up, "Up")
+scr.onkey(r_paddle.go_down, "Down")
+scr.onkey(l_paddle.go_up, "w")
+scr.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
