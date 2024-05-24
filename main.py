a93e0908fd9bf2dbc05dc13a163bf5c1803d6205
Harsha <harshahm0130@gmail.com>
2024-05-24 20:47:19 +0530

    Step-6

--- main.py
+++ main.py
@@ -22,14 +22,18 @@ scr.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.05)
+    time.sleep(0.1)
     scr.update()
     ball.move()
 
     # Detect collision with wall
     if ball.ycor() > 280 or ball.ycor() < -280:
         # Needs to bounce
-        ball.bounce()
+        ball.bounce_y()
+
+    # Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
 
 
 scr.exitonclick()
